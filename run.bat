5970969376437cfda106f5381ddef1e37d3dbe1c
Feng, Cheng-Chi <qwe987299@gmail.com>
2025-03-13 10:56:02 +0800

    更新

--- run.bat
+++ run.bat
@@ -1,9 +1,16 @@
-REM 啟動虛擬環境
 echo Activating virtual environment...
 call venv\Scripts\activate.bat
 
-REM 執行程式
 python run.py
 
-REM 刪除 src 目錄內所有檔案
 del /Q src\*.*
+
+timeout /t 1
+
+START build\
+
+echo  END!
+
+timeout /t 3
+
+exit
